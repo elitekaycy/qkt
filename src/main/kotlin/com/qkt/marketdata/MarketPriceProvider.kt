392ae1a1fbf364efa32cb2509dc64e1e61c51d42
elitekaycy <dicksonanyaele1234@gmail.com>
2026-05-03 13:06:42 +0000

    feat(marketdata): add MarketPriceProvider and tracker

---
+++ src/main/kotlin/com/qkt/marketdata/MarketPriceProvider.kt
@@ -0,0 +1,15 @@
+package com.qkt.marketdata
+
+interface MarketPriceProvider {
+    fun lastPrice(symbol: String): Double?
+}
+
+class MarketPriceTracker : MarketPriceProvider {
+    private val prices = mutableMapOf<String, Double>()
+
+    fun update(symbol: String, price: Double) {
+        prices[symbol] = price
+    }
+
+    override fun lastPrice(symbol: String): Double? = prices[symbol]
+}
