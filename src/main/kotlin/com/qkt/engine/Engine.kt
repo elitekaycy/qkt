6350eae1f931ab17ea935a44b9385d2d7b140f94
elitekaycy <dicksonanyaele1234@gmail.com>
2026-05-03 16:14:27 +0000

    refactor: route engine through event bus

--- src/main/kotlin/com/qkt/engine/Engine.kt
+++ src/main/kotlin/com/qkt/engine/Engine.kt
@@ -1,42 +1,20 @@
 package com.qkt.engine
 
-import com.qkt.broker.Broker
-import com.qkt.common.Clock
-import com.qkt.common.IdGenerator
-import com.qkt.common.Side
-import com.qkt.execution.Order
-import com.qkt.execution.OrderType
-import com.qkt.execution.Trade
+import com.qkt.bus.EventBus
+import com.qkt.events.TickEvent
 import com.qkt.marketdata.MarketPriceTracker
 import com.qkt.marketdata.Tick
-import com.qkt.strategy.Signal
-import com.qkt.strategy.Strategy
+import org.slf4j.LoggerFactory
 
 class Engine(
-    private val strategy: Strategy,
-    private val broker: Broker,
-    private val clock: Clock,
-    private val idGenerator: IdGenerator,
+    private val bus: EventBus,
     private val priceTracker: MarketPriceTracker,
-    private val onTrade: (Trade) -> Unit = {},
 ) {
+    private val log = LoggerFactory.getLogger(Engine::class.java)
+
     fun onTick(tick: Tick) {
         priceTracker.update(tick.symbol, tick.price)
-        strategy.onTick(tick) { signal -> route(signal) }
+        log.debug("ingested tick {} @ {}", tick.symbol, tick.price)
+        bus.publish(TickEvent(tick))
     }
-
-    private fun route(signal: Signal) {
-        val order = signal.toOrder(idGenerator.next(), clock.now())
-        val trade = broker.execute(order) ?: return
-        onTrade(trade)
-    }
-
-    private fun Signal.toOrder(
-        id: String,
-        ts: Long,
-    ): Order =
-        when (this) {
-            is Signal.Buy -> Order(id, symbol, Side.BUY, size, OrderType.MARKET, null, ts)
-            is Signal.Sell -> Order(id, symbol, Side.SELL, size, OrderType.MARKET, null, ts)
-        }
 }
