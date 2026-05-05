e46a8cd6fa3c3efda01a798bab0e674dc58302ee
elitekaycy <dicksonanyaele1234@gmail.com>
2026-05-05 22:55:11 +0000

    feat(common): add TradingCalendar interface and crypto calendar

---
+++ src/main/kotlin/com/qkt/common/TradingCalendar.kt
@@ -0,0 +1,31 @@
+package com.qkt.common
+
+import java.time.Instant
+
+interface TradingCalendar {
+    val name: String
+
+    fun isInSession(
+        symbol: String,
+        t: Instant,
+    ): Boolean
+
+    fun sessionRange(
+        symbol: String,
+        t: Instant,
+    ): TimeRange
+
+    fun anchorEpochFor(
+        anchor: SessionAnchor,
+        t: Instant,
+    ): Long
+
+    fun rangeFor(
+        anchor: SessionAnchor,
+        anchorEpoch: Long,
+    ): TimeRange
+
+    companion object {
+        fun crypto(): TradingCalendar = CryptoCalendar
+    }
+}
