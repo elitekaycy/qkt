536b3f3ad53fd5ca1925f594f80fb186a1f93908
elitekaycy <dicksonanyaele1234@gmail.com>
2026-05-05 10:24:42 +0000

    feat(marketdata): add HistoricalTickFeed.fromCsv factory

--- src/main/kotlin/com/qkt/marketdata/HistoricalTickFeed.kt
+++ src/main/kotlin/com/qkt/marketdata/HistoricalTickFeed.kt
@@ -1,9 +1,24 @@
 package com.qkt.marketdata
 
+import java.nio.file.Path
+
 class HistoricalTickFeed(
     private val ticks: List<Tick>,
 ) : TickFeed {
     private var index = 0
 
     override fun next(): Tick? = if (index < ticks.size) ticks[index++] else null
+
+    companion object {
+        fun fromCsv(path: Path): HistoricalTickFeed {
+            val all = mutableListOf<Tick>()
+            CsvTickFeed(path).use { feed ->
+                while (true) {
+                    val t = feed.next() ?: break
+                    all.add(t)
+                }
+            }
+            return HistoricalTickFeed(all)
+        }
+    }
 }
