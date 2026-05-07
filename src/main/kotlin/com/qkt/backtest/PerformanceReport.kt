76da3ce7d40f9e369bc03d0b9f23509c11d039a7
elitekaycy <dicksonanyaele1234@gmail.com>
2026-05-07 09:15:17 +0000

    feat(backtest): add PerformanceReport data class

---
+++ src/main/kotlin/com/qkt/backtest/PerformanceReport.kt
@@ -0,0 +1,21 @@
+package com.qkt.backtest
+
+import java.math.BigDecimal
+
+data class PerformanceReport(
+    val realizedTotal: BigDecimal,
+    val unrealizedTotal: BigDecimal,
+    val totalPnL: BigDecimal,
+    val tradeCount: Int,
+    val winRate: BigDecimal,
+    val maxDrawdown: BigDecimal,
+    val profitFactor: BigDecimal?,
+    val avgWin: BigDecimal,
+    val avgLoss: BigDecimal,
+    val largestWin: BigDecimal,
+    val largestLoss: BigDecimal,
+    val maxConsecutiveLosses: Int,
+    val sharpeRatio: BigDecimal?,
+    val calmarRatio: BigDecimal?,
+    val equityCurve: List<EquitySample>,
+)
