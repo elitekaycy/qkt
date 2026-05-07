5f54ddc69f973ea1fa0d2bcd9192d6719a6bc99e
elitekaycy <dicksonanyaele1234@gmail.com>
2026-05-07 22:29:15 +0000

    test(dsl): refresh boundary lock for 11c2

--- src/test/kotlin/com/qkt/dsl/compile/UnsupportedAstTest.kt
+++ src/test/kotlin/com/qkt/dsl/compile/UnsupportedAstTest.kt
@@ -1,11 +1,7 @@
 package com.qkt.dsl.compile
 
-import com.qkt.dsl.ast.AggFn
-import com.qkt.dsl.ast.Aggregate
-import com.qkt.dsl.ast.SinceOpen
 import com.qkt.dsl.ast.StateAccessor
 import com.qkt.dsl.ast.StateSource
-import com.qkt.dsl.ast.StreamFieldRef
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -13,14 +9,7 @@ class UnsupportedAstTest {
     private val ec = ExprCompiler()
 
     @Test
-    fun `aggregates are not supported in 11c1`() {
-        assertThatThrownBy {
-            ec.compile(Aggregate(AggFn.MAX, StreamFieldRef("btc", "close"), SinceOpen))
-        }.hasMessageContaining("unsupported")
-    }
-
-    @Test
-    fun `OPEN_ORDERS state is not supported in 11c1`() {
+    fun `OPEN_ORDERS state is not supported in 11c2`() {
         assertThatThrownBy {
             ec.compile(StateAccessor(StateSource.OPEN_ORDERS, "btc"))
         }.isInstanceOf(IllegalArgumentException::class.java)
