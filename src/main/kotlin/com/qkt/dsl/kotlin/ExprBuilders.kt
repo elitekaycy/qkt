09bf732956b5387630d4c221d2ee40d519657068
elitekaycy <dicksonanyaele1234@gmail.com>
2026-05-07 21:08:04 +0000

    feat(dsl): kotlin operator and indicator builders

---
+++ src/main/kotlin/com/qkt/dsl/kotlin/ExprBuilders.kt
@@ -0,0 +1,45 @@
+package com.qkt.dsl.kotlin
+
+import com.qkt.dsl.ast.BinOp
+import com.qkt.dsl.ast.BinaryOp
+import com.qkt.dsl.ast.Cmp
+import com.qkt.dsl.ast.CmpOp
+import com.qkt.dsl.ast.ExprAst
+import com.qkt.dsl.ast.NumLit
+import com.qkt.dsl.ast.UnOp
+import com.qkt.dsl.ast.UnaryOp
+import java.math.BigDecimal
+
+val Int.bd: ExprAst get() = NumLit(BigDecimal(this))
+val Long.bd: ExprAst get() = NumLit(BigDecimal(this))
+val Double.bd: ExprAst get() = NumLit(BigDecimal(this.toString()))
+val String.bd: ExprAst get() = NumLit(BigDecimal(this))
+val BigDecimal.bd: ExprAst get() = NumLit(this)
+
+operator fun ExprAst.plus(other: ExprAst): ExprAst = BinaryOp(BinOp.ADD, this, other)
+
+operator fun ExprAst.minus(other: ExprAst): ExprAst = BinaryOp(BinOp.SUB, this, other)
+
+operator fun ExprAst.times(other: ExprAst): ExprAst = BinaryOp(BinOp.MUL, this, other)
+
+operator fun ExprAst.div(other: ExprAst): ExprAst = BinaryOp(BinOp.DIV, this, other)
+
+operator fun ExprAst.unaryMinus(): ExprAst = UnaryOp(UnOp.NEG, this)
+
+infix fun ExprAst.gt(other: ExprAst): ExprAst = CmpOp(Cmp.GT, this, other)
+
+infix fun ExprAst.lt(other: ExprAst): ExprAst = CmpOp(Cmp.LT, this, other)
+
+infix fun ExprAst.gte(other: ExprAst): ExprAst = CmpOp(Cmp.GE, this, other)
+
+infix fun ExprAst.lte(other: ExprAst): ExprAst = CmpOp(Cmp.LE, this, other)
+
+infix fun ExprAst.eq(other: ExprAst): ExprAst = CmpOp(Cmp.EQ, this, other)
+
+infix fun ExprAst.neq(other: ExprAst): ExprAst = CmpOp(Cmp.NE, this, other)
+
+infix fun ExprAst.and(other: ExprAst): ExprAst = BinaryOp(BinOp.AND, this, other)
+
+infix fun ExprAst.or(other: ExprAst): ExprAst = BinaryOp(BinOp.OR, this, other)
+
+fun not(arg: ExprAst): ExprAst = UnaryOp(UnOp.NOT, arg)
--- src/test/kotlin/com/qkt/dsl/kotlin/StrategyBuilderTest.kt
+++ src/test/kotlin/com/qkt/dsl/kotlin/StrategyBuilderTest.kt
@@ -24,4 +24,23 @@ class StrategyBuilderTest {
             }
         assertThat(ast.streams).containsExactly(StreamDecl("btc", "BACKTEST", "BTCUSDT", "1m"))
     }
+
+    @Test
+    fun `ema helper builds an IndicatorCall`() {
+        val btc = StreamRef("btc")
+        val expr = ema(btc.close, period = 9)
+        assertThat(expr).isInstanceOf(com.qkt.dsl.ast.IndicatorCall::class.java)
+        val ic = expr as com.qkt.dsl.ast.IndicatorCall
+        assertThat(ic.name).isEqualTo("EMA")
+        assertThat(ic.args).hasSize(2)
+    }
+
+    @Test
+    fun `comparison and arithmetic operators build AST nodes`() {
+        val btc = StreamRef("btc")
+        val cond = btc.close gt 100.bd
+        assertThat(cond).isInstanceOf(com.qkt.dsl.ast.CmpOp::class.java)
+        val sum = btc.high - btc.low
+        assertThat(sum).isInstanceOf(com.qkt.dsl.ast.BinaryOp::class.java)
+    }
 }
